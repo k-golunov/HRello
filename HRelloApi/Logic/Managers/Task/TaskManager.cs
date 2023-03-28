188b6614cc641eb5c149dcda33ae1c57dcfbbb3e
Danil <donul.mukhametshin.03@gmail.com>
2023-03-28 20:17:48 +0500

    статус мэнэджер

--- HRelloApi/Logic/Managers/Task/TaskManager.cs
+++ HRelloApi/Logic/Managers/Task/TaskManager.cs
@@ -5,18 +5,9 @@ using Logic.Managers.Task.Interfaces;
 
 namespace Logic.Managers.Task;
 
-public class TaskManager: BaseManager<TaskDal, Guid>, ITaskStatusManager, ITaskManager
+public class TaskManager: BaseManager<TaskDal, Guid>, ITaskManager
 {
     public TaskManager(ITaskRepository repository) : base(repository)
     {
-    }
-    
-    public async System.Threading.Tasks.Task ChangeStatus(Guid taskId, bool isNext)
-    {
-        var task = await Repository.GetAsync(taskId);
-        if(task == null)
-            return;
-        task.Status += 1;
-        var id = await Repository.UpdateAsync(task);
     }
 }
