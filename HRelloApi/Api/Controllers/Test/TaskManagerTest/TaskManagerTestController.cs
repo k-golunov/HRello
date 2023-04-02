fcfb1a7591260243cd5518e68a415db5f9f5d07c
Danil <donul.mukhametshin.03@gmail.com>
2023-04-03 01:07:47 +0500

    Убрал статус мэнэджер, сделал универсальное получение сущности, но с костылем

---
+++ HRelloApi/Api/Controllers/Test/TaskManagerTest/TaskManagerTestController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Dal.Tasks.Entities;
+using HRelloApi.Controllers.Public.Task.dto.request;
+using Logic.Managers.Tasks.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRelloApi.Controllers.Test.TaskManagerTest;
+
+/// <summary>
+/// 
+/// </summary>
+[Route("api/v1/debug/task")]
+public class TaskManagerTestController : ControllerBase
+{
+    private readonly ITaskUnitOfWorkManager _manager;
+    private readonly IMapper _mapper;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="manager"></param>
+    public TaskManagerTestController(ITaskUnitOfWorkManager manager, IMapper mapper)
+    {
+        _manager = manager;
+        _mapper = mapper;
+    }
+    
+    [HttpPost("test")]
+    public async Task<IActionResult> Test(Guid id)
+    {
+        var a = await _manager.GetAsync(typeof(TaskDal), id) as TaskDal;
+        return Ok(a.Name);
+    }
+}
