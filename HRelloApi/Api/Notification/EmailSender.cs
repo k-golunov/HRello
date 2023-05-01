60b24d0b30403c3a0bfa1251fb212c71c4ed91e9
Kostya Golunov <kostya.golunov2015@yandex.ru>
2023-05-01 12:21:50 +0500

    Попытка отправки email сообщений

---
+++ HRelloApi/Api/Notification/EmailSender.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MimeKit;
+using Serilog;
+using Serilog.Events;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+namespace HRelloApi.Notification;
+
+/// <summary>
+/// 
+/// </summary>
+public static class EmailSender
+{
+    public static void SendEmail(string message, string mailTo)
+    {
+        try
+        {
+            var Mailmessage = new MimeMessage();
+            Mailmessage.From.Add(new MailboxAddress("Моя компания",
+                "intervalgd@mail.ru")); //отправитель сообщения поменять адрес
+            Mailmessage.To.Add(new MailboxAddress("Моя компания", mailTo)); //адресат сообщения поменять адрес
+            Mailmessage.Subject = "Сообщение от HRello"; //тема сообщения 
+            Mailmessage.Body = new BodyBuilder() { HtmlBody = $"<div style=\"color: orange;\">{message}</div>" }
+                .ToMessageBody(); //тело сообщения (так же в формате HTML)
+
+            using var client = new SmtpClient();
+            client.Connect("smtp.mail.ru", 465, true);
+            client.Authenticate("intervalgd@mail.ru", "jhmcg8hfpFJidgD3hvM2");
+            client.Send(Mailmessage);
+            client.Disconnect(true);
+            Log.Logger.Write(LogEventLevel.Information,"Сообщение успешно отправлено");
+        }
+        catch (Exception e)
+        {
+            Log.Logger.Write(LogEventLevel.Error,$"Ошибка в отправке сообщения {e.Message}");
+        }
+    }
+}
