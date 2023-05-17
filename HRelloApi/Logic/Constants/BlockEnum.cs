7d0c4f077d32e76dfa6203692fd961af9e1c2c82
Kostya Golunov <kostya.golunov2015@yandex.ru>
2023-05-17 21:13:50 +0500

    рест на дефолтное заполнение бд блоками для задач

---
+++ HRelloApi/Logic/Constants/BlockEnum.cs
@@ -0,0 +1,49 @@
+﻿namespace Logic.Constants;
+
+public static class BlockConstants
+{
+    /// <summary>
+    /// Подбор
+    /// </summary>
+    public const string Selection = "Подбор";
+
+    /// <summary>
+    /// Адаптация
+    /// </summary>
+    public const string Adaptation = "Адаптация";
+
+    /// <summary>
+    /// Развитие персонала
+    /// </summary>
+    public const string StaffDevelopment = "Развитие персонала";
+
+    /// <summary>
+    /// Hr сопровождение
+    /// </summary>
+    public const string HRSupport = "Hr сопровождение";
+
+    /// <summary>
+    /// Корпоративная  культура
+    /// </summary>
+    public const string CorporateCulture = "Корпоративная  культура";
+
+    /// <summary>
+    /// Кадровый учет и зп
+    /// </summary>
+    public const string PersonnelAccountingAndSalary = "Кадровый учет и зп";
+
+    /// <summary>
+    /// HR бренд и сопровождение
+    /// </summary>
+    public const string HRBrandExternal = "HR бренд и сопровождение";
+    
+    /// <summary>
+    /// Внутренняя работа
+    /// </summary>
+    public const string InternalWork = "Внутренняя работа";
+    
+    /// <summary>
+    /// Оценка
+    /// </summary>
+    public const string Estimation = "Оценка";
+}
