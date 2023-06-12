9073bcd18b9014e10929d3d38fe21f90b88dab66
Kostya Golunov <kostya.golunov2015@yandex.ru>
2023-06-12 21:44:29 +0500

    Привязкка листа сотрудников к отделу

---
+++ HRelloApi/Api/Controllers/Public/Departament/Dto/Request/UpdateDepartmentByUserListRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace HRelloApi.Controllers.Public.Departament.Dto.Request;
+
+/// <summary>
+/// прикреплении нескольки пользователей к отделу
+/// </summary>
+public record UpdateDepartmentByUserListRequest
+{
+    /// <summary>
+    /// идентификатор пользоавателя
+    /// </summary>
+    public required List<string> UserIdList { get; init; }
+    
+    /// <summary>
+    /// Идентификатор нового отдела
+    /// </summary>
+    public required int NewDepartmentId { get; init; }
+}
